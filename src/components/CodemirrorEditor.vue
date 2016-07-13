7355d00e16d4a2de3ec1b9821b5b3b4032f454f3
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-13 09:26:35 -0300

    Boot codemirror

---
+++ src/components/CodemirrorEditor.vue
@@ -0,0 +1,31 @@
+<template lang="jade">
+    textarea#codemirror-editor(v-model="code")
+</template>
+
+<script>
+    /* eslint-disable */
+    import CodeMirror from 'codemirror'
+    /* eslint-enable */
+
+    import 'codemirror/lib/codemirror.js'
+    import 'codemirror/lib/codemirror.css'
+    
+    export default {
+      props: ['code'],
+      ready () {
+        CodeMirror(document.getElementById('codemirror-editor'), {
+          mode: 'markdown',
+          highlightFormatting: true,
+          lineNumbers: true
+        })
+      },
+      watch: {
+        code (value) {
+          // This will be changed to vuex later
+          this.$dispatch('code', value)
+        }
+      }
+    }
+</script>
+
+<style></style>
